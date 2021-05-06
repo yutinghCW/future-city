b17354bf78822ee6355ee5d7875c2ae4c35b73db
yutingh <yutingh@cw.com.tw>
2021-05-06 15:10:31 +0800

    table

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,14 +1,22 @@
 $(function(){
+    var iPhone = (navigator.userAgent.match(/iPhone/i) != null) || (navigator.userAgent.match(/iPod/i) != null);
+	var android = (navigator.userAgent.match(/android/i) != null);
+	var iPad = navigator.userAgent.match(/iPad/i) != null;
+
+    if ( iPhone|android|iPad ) {
+        $('.tooltip').addClass('tooltip--disabled');
+    }
+
     $('#btn-hamburger').on('click', function() {
         $(this).toggleClass('icon-close');
         $('#menu').slideToggle();
         $('#search').slideUp();
         $('#btn-search').removeClass('icon-close').addClass('icon-search');
-    })
+    });
     $('#btn-search').on('click', function() {
         $(this).toggleClass('icon-search icon-close');
         $('#search').slideToggle();
         $('#menu').slideUp();
         $('#btn-hamburger').removeClass('icon-close');
-    })
+    });
 });
